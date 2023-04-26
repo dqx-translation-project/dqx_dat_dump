cf6fedc2bc0655c03bd2d13d18f3a6d72c61f9d9
jmctune <17505625+jmctune@users.noreply.github.com>
2023-04-25 20:49:48 -0500

    break main() out into functions; merge encrypt[raw] and decrypt[raw] functions; allow manually creating and disabling agent for bulk operations without multiple attaching.

--- tools/dump_etps/dqxcrypt/dqxcrypt.py
+++ tools/dump_etps/dqxcrypt/dqxcrypt.py
@@ -1,3 +1,4 @@
+import argparse
 from collections import namedtuple
 import io
 import struct
@@ -6,12 +7,16 @@ import frida
 from frida_agent import FridaAgent
 from managed_package_data_client import ManagedPackageDataClient
 
+
 CryFile = namedtuple('CryFile', ['magic', 'version', 'unknown', 'data'])
+
+
 def read_cry_file(filepath: str) -> CryFile:
     with open(filepath, 'rb') as f:
         (magic, data_size, version, unk0) = struct.unpack('<IIII', f.read(16))
         data = f.read(data_size)
         return CryFile(magic, version, unk0, data)
+
 
 def write_cry_file(cryfile: CryFile) -> bytes:
     out = io.BytesIO()
@@ -19,19 +24,6 @@ def write_cry_file(cryfile: CryFile) -> bytes:
     out.write(cryfile.data)
     return out.getvalue()
 
-
-
-def print_usage():
-    print("""DQX CRY [en|de]cryptor
-    
-Usage:
-    * dqxcrypt.py encrypt <path to unencrypted CRY file> <encryption key string>
-    * dqxcrypt.py encrypt_raw <path to unencrypted raw file> <encryption key string>
-    * dqxcrypt.py decrypt <path to encrypted CRY file> <encryption key string>
-    * dqxcrypt.py decrypt_raw <path to encrypted raw file> <encryption key string>
-    * dqxcrypt.py bruteforce <path to encrypted CRY file> <path to ManagedPackageDataClient.win32.pkg>
-    * dqxcrypt.py logger
-    """)
 
 def do_encrypt(agent, filepath, encryption_key, rawFile=False):
     with open(filepath, 'rb') as f:
@@ -41,6 +33,7 @@ def do_encrypt(agent, filepath, encryption_key, rawFile=False):
             return encrypted_data
         else:
             return write_cry_file(CryFile(0x9595243, 16, 0, encrypted_data))
+
 
 def do_decrypt(agent, filepath, encryption_key, rawFile=False):
     data = None
@@ -53,6 +46,7 @@ def do_decrypt(agent, filepath, encryption_key, rawFile=False):
 
     decrypted_data = agent.blowfish_decrypt(encryption_key, data)
     return (encryption_key, decrypted_data)
+
 
 def do_bruteforce_decrypt(agent, filepath, managed_package_data_client_path, crib=b'EVTX'):
     # Read the encrypted data + key file
@@ -85,84 +79,114 @@ def do_bruteforce_decrypt(agent, filepath, managed_package_data_client_path, cri
 
     return (None, None)
 
-def main():
-    # Terrible CLI parsing, but it works, so meh.
-    if len(sys.argv) < 2:
-        print_usage()
-        exit(1)
-    elif sys.argv[1] not in ('encrypt', 'encrypt_raw', 'decrypt', 'decrypt_raw', 'bruteforce', 'logger'):
-        print_usage()
-        exit(1)
-    elif (sys.argv[1] == 'encrypt' or sys.argv[1] == 'encrypt_raw') and len(sys.argv) < 4:
-        print_usage()
-        exit(1)
-    elif (sys.argv[1] == 'decrypt' or sys.argv[1] == 'decrypt_raw') and len(sys.argv) < 4:
-        print_usage()
-        exit(1)
-    elif sys.argv[1] == 'bruteforce' and len(sys.argv) < 4:
-        print_usage()
-        exit(1)
 
-
-    print('Attaching to game client...')
-
-    # Attach to the game
+def attach_client() -> object:
     try:
         agent = FridaAgent()
         agent.attach_game()
     except frida.ProcessNotFoundError:
         sys.exit("Could not find process DQXGame.exe. Cannot continue.")
 
-    if (sys.argv[1] == 'encrypt' or sys.argv[1] == 'encrypt_raw'):
-        is_raw = sys.argv[1].endswith('raw')
-        filepath = sys.argv[2]
-        encryption_key = sys.argv[3]
-        
-        print(f'Encrypting {filepath} with key {encryption_key}')
-        data = do_encrypt(agent, filepath, encryption_key, rawFile=is_raw)
+    return agent
 
-        output_filepath = filepath + '.enc'
-        print(f'Encrypted with key "{encryption_key}". Writing to: {output_filepath}')
-        with open(output_filepath, 'wb') as f:
-            f.write(data)
 
-    
-    elif (sys.argv[1] == 'decrypt' or sys.argv[1] == 'decrypt_raw'):
-        is_raw = sys.argv[1].endswith('raw')
-        filepath = sys.argv[2]
-        encryption_key = sys.argv[3]
+def cli_logger(agent: object):
+    agent.init_logging()
+    agent.install_hash_logger()
+    agent.install_blowfish_logger()
+    print("[!] Press any to stop logging\n\n")
+    sys.stdin.read()
 
-        (key, decrypted_data) = do_decrypt(agent, filepath, encryption_key, rawFile=is_raw)
+
+def cli_encrypt(agent: object, filepath: str, encryption_key: str):
+    is_raw = True
+    if ".win32." in filepath:
+        is_raw = False  # etp files with '.win32.' are CRY files. found in the RPS
+
+    print(f'Encrypting {filepath} with key {encryption_key}')
+    data = do_encrypt(agent, filepath, encryption_key, rawFile=is_raw)
+
+    output_filepath = filepath + '.enc'
+    print(f'Encrypted with key "{encryption_key}". Writing to: {output_filepath}')
+    with open(output_filepath, 'wb') as f:
+        f.write(data)
+
+
+def cli_decrypt(agent: object, filepath: str, encryption_key: str):
+    is_raw = True
+    with open(filepath, "rb") as f:
+        data = f.read(4)
+        if data == b"\x43\x52\x59\x09": # CRY
+            is_raw = False
+
+    (key, decrypted_data) = do_decrypt(agent, filepath, encryption_key, rawFile=is_raw)
+    output_filepath = filepath + '.dec'
+    print(f'Decrypted with key "{key}". Writing to: {output_filepath}')
+    with open(output_filepath, 'wb') as f:
+        f.write(decrypted_data)
+
+
+def cli_bruteforce(agent: object, filepath: str, managed_package_data_client_path: str):
+    print(f'Attempting decryption of {filepath} with keys from {managed_package_data_client_path}.')
+    (key, data) = do_bruteforce_decrypt(agent, filepath, managed_package_data_client_path)
+
+    # Write file if we managed to decrypt it.
+    if key != None:
         output_filepath = filepath + '.dec'
         print(f'Decrypted with key "{key}". Writing to: {output_filepath}')
         with open(output_filepath, 'wb') as f:
-            f.write(decrypted_data)
+            f.write(data)
+    else:
+        print('Failed to decrypt file with the keys in ManagedPackageDataClient.win32.pkg')
 
-    elif sys.argv[1] == 'bruteforce':
-        filepath = sys.argv[2]
-        managed_package_data_client_path = sys.argv[3]
 
-        print(f'Attempting decryption of {filepath} with keys from {managed_package_data_client_path}.')
-        (key, data) = do_bruteforce_decrypt(agent, filepath, managed_package_data_client_path)
+def print_help(parser: object):
+    parser.print_help()
+    sys.exit(0)
 
-        # Write file if we managed to decrypt it.
-        if key != None:
-            output_filepath = filepath + '.dec'
-            print(f'Decrypted with key "{key}". Writing to: {output_filepath}')
-            with open(output_filepath, 'wb') as f:
-                f.write(data)
-        else:
-            print('Failed to decrypt file with the keys in ManagedPackageDataClient.win32.pkg')
-
-    elif sys.argv[1] == 'logger':
-        agent.init_logging()
-        agent.install_hash_logger()
-        agent.install_blowfish_logger()
-        print("[!] Press any to stop logging\n\n")
-        sys.stdin.read()
-
-    # Detach from game
-    agent.detach_game()
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="DQX [en|de]cryptor")
+    parser.add_argument(
+        "action", choices=["encrypt", "decrypt", "bruteforce", "logger"],
+        help="Specify an action."
+    )
+    parser.add_argument(
+        "--file", nargs="?", const="arg_not_given",
+        help="(opt) File to [en|de]crypt."
+    )
+    parser.add_argument(
+        "--encryption-key", nargs="?", const="arg_not_given",
+        help="(opt) Encryption key."
+    )
+    parser.add_argument(
+        "--mpdc", nargs="?", const="arg_not_given",
+        help="(opt) Path to the ManagedPackageDataClient file (for CRY files.)"
+    )
+
+    args = parser.parse_args()
+
+    if not args.action:
+        print_help(parser)
+
+    agent = attach_client()
+
+    if args.action == "encrypt":
+        if not args.file or not args.encryption_key:
+            print_help(parser)
+        cli_encrypt(agent=agent, filepath=args.file, encryption_key=args.encryption_key)
+
+    if args.action == "decrypt":
+        if not args.file or not args.encryption_key:
+            print_help(parser)
+        cli_decrypt(agent=agent, filepath=args.file, encryption_key=args.encryption_key)
+
+    if args.action == "bruteforce":
+        if not args.file or not args.mpdc:
+            print_help(parser)
+        cli_bruteforce(agent=agent, filepath=args.file, managed_package_data_client_path=args.mpdc)
+
+    if args.action == "logger":
+        cli_logger(agent=agent)
+
+    agent.detach_game()
