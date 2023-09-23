b18c7d41c046d4416c7d9e20bb677e01125b87ca
Serany <17505625+jmctune@users.noreply.github.com>
2023-09-22 19:02:08 -0500

    - Remove hardcoded size globals and just reference 0x400 for reads. - Don't read all file at once and parse it. Just read the file from   top to bottom, parsing along the way. - Convert several methods to private - Add num_dats to segments and cleans up some wrong info - Look up each record in the dat_db and append it to the record. Allows   for identification, but makes loading the file much slower as it now   makes thousands of db calls while it's looking this data up - Implement looking up records by either filename or hashed filename

--- tools/lib/idxfile.py
+++ tools/lib/idxfile.py
@@ -1,10 +1,12 @@
 from binascii import hexlify
 from struct import unpack, pack
+import sys
+import pathlib
+from fileops import pack_uint
+import sqlite3
 
-SMPK_SIZE = 0x400
-STRUCT_SIZE = 0x400
 
-file_types = {
+FILE_TYPES = {
     1: "SQDB?",
     2: "Data",
     3: "Index"
@@ -15,34 +17,47 @@ class IdxFile:
     def __init__(self, file: str):
         self.file = file
 
+        self.__is_idx(file=self.file)
+
         # idx files are pretty small (less than 4MB), so read all in
         with open(file, "rb") as f:
-            file_data = f.read()
+            self.smpk = self.__read_smpk(f.read(0x400))
+            self.segments = self.__read_segments(f.read(0x400))
 
-        self.smpk = self.smpk(
-            buf=file_data[0:SMPK_SIZE])
-        self.segments = self.segments(
-            buf=file_data[SMPK_SIZE:SMPK_SIZE+STRUCT_SIZE])
-        self.records = self.records(
-            buf=file_data[SMPK_SIZE+STRUCT_SIZE:self.segments[1]["size"]]
-        )
+            # only read in the files, which are at the top of the record list.
+            self.records = self.__read_all_records(f.read(self.segments[1]["size"]))
 
-    def smpk(self, buf):
+
+    def __is_idx(self, file: str):
+        """
+        Check if the file provided is a valid IDX file.
+        """
+        if pathlib.Path(file).suffix != ".idx":
+            print("File extension is not '.idx'.")
+            sys.exit(1)
+
+        with open(file, "rb") as f:
+            if f.read(4) != b"\x53\x4D\x50\x4B":  # SMPK
+                print("Unrecognized IDX header. Did not read SMPK.")
+                sys.exit(1)
+
+
+    def __read_smpk(self, buf):
         data = unpack("<4s 8x I 4x I 936x 20s 44x", buf)
         header = {
             "signature": data[0],
             "length": data[1],
-            "type": file_types[data[2]],
+            "type": FILE_TYPES[data[2]],
             "sha1": hexlify(data[3])
         }
 
         return header
 
-    def segments(self, buf):
+
+    def __read_segments(self, buf):
         data = unpack("<I I I I 20s 44x I I I 20s 44x I I I 16s 44x I I I 16s 704x 20s 44x", buf)
         segments = {
             "length": data[0],
-            "num_dats": data[5],
             "sha1": hexlify(data[17]),
             1: {  # files
                 "unknown": data[1],
@@ -51,27 +66,39 @@ class IdxFile:
                 "sha1": hexlify(data[4]),
             },
             2: {
+                "num_dats": data[5],  # controls how many dats to load. 0x01 = .dat0, 0x02 = .dat0 + .dat1, etc.
                 "offset": data[6],
                 "size": data[7],
                 "sha1": hexlify(data[8]),
             },
             3: {
-                "unknown": data[9],
-                "offset": data[10],
-                "size": data[11],
+                "offset": data[9],
+                "size": data[10],
+                "unknown": data[11],
                 "sha1": hexlify(data[12]),
             },
             4: {  # folders
-                "unknown": data[13],
-                "offset": data[14],
-                "size": data[15],
+                "offset": data[13],
+                "size": data[14],
+                "unknown": data[15],
                 "sha1": hexlify(data[16]),
-            }
+            },
         }
 
         return segments
 
-    def records(self, buf):
+
+    def __db_lookup(self, hashed_filename: str):
+        db_path = "../../dat_db.db"
+        db_conn = sqlite3.connect(db_path)
+        db_cur = db_conn.cursor()
+        db_result = db_cur.execute(f"SELECT file, directory FROM files WHERE file_dir_hash = '{hashed_filename}'")
+        found = db_result.fetchone()
+        db_conn.close()
+        return found or ("", "")
+
+
+    def __read_all_records(self, buf):
         rows = buf
         count = 0
         position = 0
@@ -88,16 +115,21 @@ class IdxFile:
 
             # using as an identifier to figure out which row in the index something is in
             # since I don't have filenames to work with. this just outputs the first 8 bytes
-            # of the index entry.
-            filename = (hexlify(pack("<L", data[0])) + hexlify(pack("<L", data[1]))).decode()
+            # of the index entry, which is the hashed file name + folder name.
+            hashed_filename = (hexlify(pack("<L", data[0])) + hexlify(pack("<L", data[1]))).decode()
+
+            db_result = self.__db_lookup(hashed_filename=hashed_filename)
 
             record = {
-                "file_hash": data[0],
-                "folder_hash": data[1],
-                "idx_offset": SMPK_SIZE + STRUCT_SIZE + position,
+                "file_hash": pack_uint(data[0]).hex(),
+                "folder_hash": pack_uint(data[1]).hex(),
+                "idx_offset": 0x800 + position,
                 "dat_offset": (data[2] & ~0xF) * 0x08,
-                "dat_num": str((data[2] & 0b1110) >> 1),
-                "filename": filename
+                "dat_num": (data[2] & 0b1110) >> 1,
+                "dat_file": pathlib.Path(self.file).stem + ".dat" + str((data[2] & 0b1110) >> 1),
+                "hashed_filename": hashed_filename,
+                "filename": db_result[0],
+                "folder": db_result[1],
             }
 
             records.append(record)
@@ -105,3 +137,22 @@ class IdxFile:
             position += 16
 
         return record_dict
+
+
+    def get_record(self, filename: str):
+        """
+        Gets a record by its actual filename.
+        """
+        for record in self.records["records"]:
+            if record["filename"] == filename:
+                return record
+        return None
+
+
+    def get_record_by_hash(self, hashed_filename: str):
+        """
+        Gets a record by its hashed filename.
+        """
+        for record in self.records["records"]:
+            if record["hashed_filename"] == hashed_filename:
+                return record
