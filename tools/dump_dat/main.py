77d836eced31821b5e6e9b75cb48847a9aed56c6
Serany <17505625+jmctune@users.noreply.github.com>
2023-11-26 13:12:26 -0600

    clean up

--- tools/dump_dat/main.py
+++ tools/dump_dat/main.py
@@ -5,7 +5,6 @@ from tools.py_globals import GAME_DATA_DIR
 from tools.lib.extensions import EXTENSIONS
 from tools.lib.idxfile import IdxFile
 from tools.lib.datentry import DatEntry
-from tools.lib.rpsfile import RpsFile
 
 
 # update to the idx file you want to dump
@@ -18,29 +17,25 @@ def unpack_idx():
 
     for record in idx.records:
         dat_file = Path(f"{GAME_DATA_DIR}/{record['dat_file']}")
-        dat = DatEntry(dat_file=dat_file, offset=record["dat_offset"])
+        dat = DatEntry(dat_file=dat_file, offset=record['dat_offset'])
         file_data = dat.data
 
         if file_data:
-            if record["filename"] and record["folder"]:
+            if record['filename'] and record['folder']:
                 filename = f"{record['folder']}/{record['filename']}"
             else:
                 ext = file_data[0:7]
                 if ext in EXTENSIONS:
-                    filename = record["hashed_filename"] + EXTENSIONS[ext]
+                    filename = record['hashed_filename'] + EXTENSIONS[ext]
                 else:
-                    filename = record["hashed_filename"]
+                    filename = record['hashed_filename']
 
-            to_write = "/".join(["out", record['dat_file'], filename])
+            to_write = '/'.join(['out', record['dat_file'], filename])
             Path(to_write).parent.mkdir(parents=True, exist_ok=True)
 
-            with open(to_write, "wb") as f:
+            with open(to_write, 'wb') as f:
                 f.write(file_data)
 
-            # if filename.split(".")[-1] == "rps":
-            #     rps = RpsFile(f"{os.getcwd()}\\out\\{dat_file_name}\\{filename}")
-            #     rps.dump()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unpack_idx()
