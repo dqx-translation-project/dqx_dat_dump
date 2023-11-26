8ab267736fbf1e331f406af5c5075d6999fce4aa
Serany <17505625+jmctune@users.noreply.github.com>
2023-11-26 13:12:52 -0600

    Add functions to compute the crc32 of a filename, folder path or both together.

--- tools/lib/fileops.py
+++ tools/lib/fileops.py
@@ -1,7 +1,7 @@
 import functools
 import itertools
 from struct import unpack, pack
-from pathlib import Path
+import zlib
 
 
 def unpack_ushort(buf: bytes) -> int:
@@ -40,3 +40,32 @@ def read_cstr(f: object) -> str:
     """
     toeof = iter(functools.partial(f.read, 1), '')
     return ''.join(itertools.takewhile('\0'.__ne__, toeof))
+
+
+def compute_crc32(path_name: str):
+    """
+    Computes a file or folder path into a CRC-32/JAMCRC dec/hex tuple.
+    This is primarily used when interacting with IDX files.
+
+    :param path_name: Path of the file name or folder path.
+    """
+    # path cannot have a forward slash at the end of the path
+    # and must also be fully lowercase for accurate result.
+    new_path_name = path_name.rstrip('/').lower().encode()
+    crc32_jamcrc = int('0b' + '1' * 32, 2) - zlib.crc32(new_path_name)
+
+    return (crc32_jamcrc, hex(crc32_jamcrc))
+
+
+def compute_crc32_full(folder_path: str, filename: str):
+    """
+    Computes the full path of a file into a CRC-32/JAMCRC dec/hex tuple.
+    This is primarily used when interacting with IDX files.
+
+    :param folder_path: Path to the folder.
+    :param filename: Name of the file.
+    """
+    folder_crc, file_crc = compute_crc32(folder_path), compute_crc32(filename)
+    calculated_crc = folder_crc[0] << 32 | file_crc[0]
+
+    return (calculated_crc, hex(calculated_crc))
